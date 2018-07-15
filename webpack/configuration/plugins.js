eda63f7c86d25e81e44f35e0c879c314284512eb
sh4d1t0 <shadito@live.com.mx>
2018-07-15 04:46:19 -0500

    Update dependencies, add cssnano, production optimization

--- webpack/configuration/plugins.js
+++ webpack/configuration/plugins.js
@@ -1,21 +1,47 @@
 // dependencies
+import CompressionPlugin from 'compression-webpack-plugin'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import webpack from 'webpack'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 // enviroment
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
-export default () => {
+// Analyzer
+const isAnalyzer = process.env.ANALYZER === 'true'
+
+export default type => {
   const plugins = [
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
     new MiniCssExtractPlugin({
       filename: '../../public/css/style.css'
     })
   ]
 
+  if (isAnalyzer) {
+    plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'static' }))
+  }
+
   if (isDevelopment) {
     plugins.push(
       new webpack.HotModuleReplacementPlugin(),
       new webpack.NoEmitOnErrorsPlugin()
+    )
+  } else {
+    plugins.push(
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.AggressiveMergingPlugin(),
+      new CompressionPlugin({
+        asset: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: /\.js$|\.css$|\.html$/,
+        threshold: 10240,
+        minRatio: 0.8
+      })
     )
   }
 
