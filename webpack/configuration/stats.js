97a89684918f76e61794847a84ec2e6763f88189
sh4d1t0 <shadito@live.com.mx>
2018-07-27 14:59:50 -0500

    Webpack performance

--- webpack/configuration/stats.js
+++ webpack/configuration/stats.js
@@ -2,20 +2,7 @@
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 export default () => {
-  const stats = isDevelopment ? {} : 'none'
-
-  if (isDevelopment) {
-    stats.push({
-      colors: true,
-      hash: true,
-      timings: true,
-      assets: true,
-      chunks: true,
-      chunkModules: true,
-      modules: true,
-      children: true
-    })
-  }
+  const stats = isDevelopment ? 'verbose' : 'none'
 
   return stats
 }
