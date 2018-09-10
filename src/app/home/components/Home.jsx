4a41485191f87d71b462e15d5d0a3db04a956d0f
sh4d1t0 <shadito@live.com.mx>
2018-09-10 02:24:15 -0500

    Testing with material ui

--- src/app/home/components/Home.jsx
+++ src/app/home/components/Home.jsx
@@ -2,6 +2,7 @@
 // dependencies
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import Button from '@material-ui/core/Button'
 
 // styles
 import styles from './Home.scss'
@@ -18,10 +19,15 @@ class Home extends Component<Props, State> {
   render(): any {
     return (
       <div className={styles.home}>
-        {'Home - '}
-        <Link to="/about">About</Link>
+        <Button color="primary">Home</Button>
         {' - '}
-        <Link to="/blog">Blog</Link>
+        <Link to="/about">
+          <Button>About</Button>
+        </Link>
+        {' - '}
+        <Link to="/blog">
+          <Button>Blog</Button>
+        </Link>
       </div>
     )
   }
