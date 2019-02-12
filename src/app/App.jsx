e48f9177c41f1f5faacdcb68fdb8dea1175dbc58
sh4d1t0 <shadito@live.com.mx>
2019-02-12 16:08:36 -0600

    update lint-staged commands

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { BrowserRouter, StaticRouter, Switch, Route } from 'react-router-dom'
 import { hot } from 'react-hot-loader/root'
-import routes from '@routes'
+import routess from '@routes'
 /* import ContextStore from 'Features/blog' */
 // routes
 
