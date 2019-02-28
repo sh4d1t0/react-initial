e2c7938323266fcac134cd77d01863644e50ea76
Shadito <shadito@live.com.mx>
2019-02-27 23:17:20 -0600

    Jest, config and first test

---
+++ src/app/views/Blog/blog.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import Blog from '@views/Blog'
+import Navbar from '@components/Navbar'
+
+configure({ adapter: new Adapter() })
+
+describe('Check login section', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallow(<Blog />)
+  })
+
+  it('should render <LoginForm /> element to auth', () => {
+    expect(wrapper.find(Navbar).children()).toBeTruthy()
+  })
+})
